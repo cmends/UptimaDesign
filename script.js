a08531ef6dc028f2cf1e0b91498dcc63eec8df78
cmends <91956652+cmends@users.noreply.github.com>
2022-05-20 17:14:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let navbar = document.getElementById("nav");
+let links = document.getElementById("links");
+let layer1 = document.querySelector(".layer1");
+let layer2 = document.querySelector(".layer2");
